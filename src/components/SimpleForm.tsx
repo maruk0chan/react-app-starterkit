f2b95307caa8d4fd0f39127ddeef074394eead3b
maruk0chan <marcochan94@hotmail.com>
2025-05-20 16:34:24 +0800

    feat: enable switching form page

--- src/components/SimpleForm.tsx
+++ src/components/SimpleForm.tsx
@@ -1,33 +1,47 @@
-import { Form } from "@formio/react";
+import { Form, type FormSource } from "@formio/react";
 import { useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import section_a1 from "./sample-input-json/session-a1-BQ17005.json";
 import section_a6 from "./sample-input-json/section-a6-BQ17019.json";
 
-const jsonMap = {
-    section_a1,
-    section_a6,
-};
+interface Page {
+    id: string;
+    label: string;
+    src: FormSource;
+}
+const pages: Page[] = [
+    {
+        id: "section_a1",
+        label: "session-a1-BQ17005.json",
+        src: section_a1 as FormSource,
+    },
+    {
+        id: "section_a6",
+        label: "section-a6-BQ17019.json",
+        src: section_a6 as FormSource,
+    },
+];
 
 const SimpleForm = () => {
-    const [src, setSrc] = useState(section_a1);
+    const [page, setPage] = useState<Page>(null);
 
     return (
         <>
             <Row>
                 <Col>
-                    <select onChange={(e) => setSrc(jsonMap?.[e.target.value])}>
-                        <option value={"section_a1"}>
-                            session-a1-BQ17005.json
-                        </option>
-                        <option value={"section_a6"}>
-                            section-a6-BQ17019.json
-                        </option>
+                    <select
+                        onChange={(e) =>
+                            setPage(pages.find((p) => p.id === e.target.value)!)
+                        }
+                    >
+                        {pages.map((p) => (
+                            <option value={p.id}>{p.label}</option>
+                        ))}
                     </select>
                     <h2>session-a1-BQ17005.json</h2>
 
                     <div className="py-3">
-                        <Form src={src} />
+                        {page && <Form src={page?.src} />}
                     </div>
                 </Col>
             </Row>
