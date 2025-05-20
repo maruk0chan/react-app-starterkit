b240942304f32185bdb23d3fe8f17aee1d344130
maruk0chan <marcochan94@hotmail.com>
2025-05-20 16:26:27 +0800

    feat: simple form dropdown

--- src/components/SimpleForm.tsx
+++ src/components/SimpleForm.tsx
@@ -1,27 +1,33 @@
 import { Form } from "@formio/react";
+import { useState } from "react";
 import { Col, Row } from "react-bootstrap";
-import { atomOneLight, CopyBlock } from "react-code-blocks";
+import section_a1 from "./sample-input-json/session-a1-BQ17005.json";
+import section_a6 from "./sample-input-json/section-a6-BQ17019.json";
+
+const jsonMap = {
+    section_a1,
+    section_a6,
+};
 
 const SimpleForm = () => {
+    const [src, setSrc] = useState(section_a1);
+
     return (
         <>
             <Row>
                 <Col>
-                    <h2>JSON Powered Forms</h2>
-                    <p>
-                        Form.io is a platform for JSON powered forms within
-                        React. Forms can be embedded as easy as the following
-                        code.
-                    </p>
-                    <CopyBlock
-                        text={`<Form src={'https://examples.form.io/example'} />`}
-                        theme={atomOneLight}
-                        language="jsx"
-                        codeBlock={true}
-                        showLineNumbers={false}
-                    />
+                    <select onChange={(e) => setSrc(jsonMap?.[e.target.value])}>
+                        <option value={"section_a1"}>
+                            session-a1-BQ17005.json
+                        </option>
+                        <option value={"section_a6"}>
+                            section-a6-BQ17019.json
+                        </option>
+                    </select>
+                    <h2>session-a1-BQ17005.json</h2>
+
                     <div className="py-3">
-                        <Form src="https://examples.form.io/example" />
+                        <Form src={src} />
                     </div>
                 </Col>
             </Row>
